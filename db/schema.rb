25280d74dacc8e1d9e4dc12346b4837ec45df217
ShariqueMohd <1sharique1@gmail.com>
2020-05-19 14:14:30 +0530

    added due date/deadline column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_19_021217) do
+ActiveRecord::Schema.define(version: 2020_05_19_084257) do
 
   create_table "todo_items", force: :cascade do |t|
     t.string "content"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_05_19_021217) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "priority", default: 0
+    t.date "deadline"
     t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
   end
 
